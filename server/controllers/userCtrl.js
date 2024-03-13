86390f9203609fa6bc603b4dfc77eab9d657a0ef
Nanahawaw <folarinyetunde@gmail.com>
2024-03-13 22:52:44 +0100

    added Oauth signin api route

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -56,6 +56,50 @@ const signIn = async (req, res) => {
   }
 };
 
+//sign in with google
+
+const google = async (req, res, next) => {
+  try {
+    const user = await user.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      //destructuring the password
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie("accessToken", token, {
+          httpOnly: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        })
+        .status(200)
+        .json({ rest, message: "You are Logged in" });
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedpassword = await bcrypt.hash(generatedPassword, 10);
+      const newUser = new User({
+        name:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
+        email: req.body.email,
+        password: hashedpassword,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("accessToken", token, {
+          httpOnly: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        })
+        .status(200)
+        .json({ rest, message: "You are Logged in" });
+    }
+  } catch (error) {
+    res.status(500).json("Internal server error");
+  }
+};
+
 //update a user
 const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
@@ -84,4 +128,4 @@ const updateUser = async (req, res, next) => {
 
 //delete a user
 
-module.exports = { signUp, signIn, updateUser };
+module.exports = { signUp, signIn, updateUser, google };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { signUp, signIn, updateUser } = require("../controllers/userCtrl.js");
+const {
+  signUp,
+  signIn,
+  updateUser,
+  google,
+} = require("../controllers/userCtrl.js");
 const router = express.Router();
 const verifyToken = require("../middlewares/verifyToken.js");
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.post("/google", google);
 router.post("/update/:id", verifyToken, updateUser);
 
 module.exports = router;
