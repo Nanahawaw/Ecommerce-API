ac3bbd7268381df28f9bea5bddf78ec2c19a8196
Nanahawaw <folarinyetunde@gmail.com>
2024-03-15 22:07:01 +0100

    fixed usermodel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,27 +1,35 @@
 const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 //Export the model
 module.exports = mongoose.model("User", userSchema);
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -10,11 +10,16 @@ const {
 const router = express.Router();
 const verifyToken = require("../middlewares/verifyToken.js");
 
+//auth routes
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.post("/google", google);
 router.get("/signout", signOut);
+
+//user routes
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+
+//admin routes
 
 module.exports = router;
