fe06f000974b56ab3c3e5138fc71ca8a948e5107
Nanahawaw <folarinyetunde@gmail.com>
2024-03-23 19:51:57 +0100

    category routes implemented

--- server/controllers/categoryCtrl.js
+++ server/controllers/categoryCtrl.js
@@ -20,4 +20,57 @@ const createCategory = async (req, res) => {
   }
 };
 
-module.exports = createCategory;
+const updateCategory = async (req, res) => {
+  try {
+    const { categoryId } = req.params;
+    const category = await Category.findOne({ _id: categoryId });
+    if (!category) {
+      return res.status(400).json('category not found');
+    } else {
+      const { name } = req.body;
+      category.name = name;
+      const updatedCategory = await category.save();
+      res.json(updatedCategory);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  try {
+    const removed = await Category.findByIdAndDelete(req.params.categoryId);
+    res.json({ message: 'category deleted successfully', removed });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+};
+
+const listAllCategories = async (req, res) => {
+  try {
+    const getallCategories = await Category.find({});
+    res.json(getallCategories);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+};
+
+const getOneCategory = async (req, res) => {
+  try {
+    const category = await Category.findById({ _id: req.params.id });
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+};
+module.exports = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  listAllCategories,
+  getOneCategory,
+};
--- server/routes/categoryRoute.js
+++ server/routes/categoryRoute.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const { verifyToken, authorizedAdmin } = require('../middlewares/verifyToken');
-const createCategory = require('../controllers/categoryCtrl.js');
+const {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  listAllCategories,
+  getOneCategory,
+} = require('../controllers/categoryCtrl.js');
 
 router.post('/', verifyToken, authorizedAdmin, createCategory);
+router.put('/:categoryId', verifyToken, authorizedAdmin, updateCategory);
+router.delete('/:categoryId', verifyToken, authorizedAdmin, deleteCategory);
+router.get('/categories', listAllCategories);
+router.get('/:id', getOneCategory);
 
 module.exports = router;
