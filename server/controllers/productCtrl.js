e0d1679109499426d0fcf0dbf20ce90b21082654
Nanahawaw <folarinyetunde@gmail.com>
2024-03-24 14:59:31 +0100

    get all products api

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -77,4 +77,75 @@ const updateProductDetails = async (req, res) => {
   }
 };
 
-module.exports = { addProduct, updateProductDetails };
+const removeProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id)
+    if (!product) { return res.status(404).json("Product not found") } else { res.json("product removed successfully") }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error.message);
+  }
+}
+const fetchProducts = async (req, res) => {
+  try {
+    const pageSize = 6;
+    const page = Number(req.query.pageNumber) || 1;
+    const keyword = req.query.keyword
+      ? {
+        name: { $regex: req.query.keyword, $options: "i" },
+      }
+      : {};
+
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword })
+      .limit(pageSize)
+      .skip(pageSize * (page - 1));
+
+    res.json({
+      products,
+      page,
+      pages: Math.ceil(count / pageSize),
+      hasmore: page < Math.ceil(count / pageSize),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+const getOneProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      return res.json(product)
+    } else {
+      res.status(404).json("product not found")
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error.message);
+  }
+}
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({}).populate("category").limit(12).sort({ createAt: -1 })
+
+    res, json(products)
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error.message);
+  }
+}
+
+module.exports = {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  getOneProduct,
+  getAllProducts
+};
