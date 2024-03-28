0c5b177a76225a5278242eaaa49dbd2610228d5c
Nanahawaw <folarinyetunde@gmail.com>
2024-03-28 14:23:54 +0100

    orders ApI completed

--- server/controllers/orderCtrl.js
+++ server/controllers/orderCtrl.js
@@ -49,7 +49,7 @@ const createOrder = async (req, res) => {
 
     const order = new Order({
       orderItems: dbOrderItems,
-      user: req.user._id,
+      user: req.user.id,
       shippingAddress,
       paymentMethod,
       itemsPrice,
@@ -63,4 +63,132 @@ const createOrder = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-module.exports = { createOrder };
+
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({}).populate('user', 'id name');
+
+    if (!orders) {
+      res.status(404).json({ error: 'No orders found' });
+    } else {
+      res.status(200).json(orders);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getUserOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ user: req.user.id });
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const countTotalOrders = async (req, res) => {
+  try {
+    const totalOrders = await Order.countDocuments();
+    res.json(totalOrders);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+const calculateTotalSales = async (req, res) => {
+  try {
+    const orders = await Order.find();
+    const totalSales = orders.reduce((acc, order) => acc + order.totalPrice, 0);
+    res.json(totalSales);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const calculateTotalSalesByDate = async (req, res) => {
+  try {
+    const salesByDate = await Order.aggregate([
+      {
+        $match: {
+          isPaid: true,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: '%Y-%m-%d',
+              date: '$paiddAt',
+            },
+          },
+          totalSales: {
+            $sum: '$totalPrice',
+          },
+        },
+      },
+    ]);
+    res.json(salesByDate);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const findOrderById = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    );
+    if (!order) {
+      res.status(404).json({ error: 'Order not found' });
+    } else {
+      res.status(200).json(order);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const markOrderAsPaid = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.payer.email_address,
+      };
+      await order.save();
+      res.status(200).json(order);
+    } else {
+      res.status(404).json({ error: 'Order not found' });
+    }
+  } catch (error) {}
+};
+const markOrderAsDelivered = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+      await order.save();
+      res.status(200).json(order);
+    } else {
+      res.status(404).json({ error: 'Order not found' });
+    }
+  } catch (error) {}
+};
+module.exports = {
+  createOrder,
+  calculateTotalSales,
+  countTotalOrders,
+  getAllOrders,
+  getUserOrders,
+  calculateTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
+};
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const { verifyToken, authorizedAdmin } = require('../middlewares/verifyToken');
-const { createOrder } = require('../controllers/orderCtrl');
+const {
+  createOrder,
+  getAllOrders,
+  getUserOrders,
+  countTotalOrders,
+  calculateTotalSales,
+  calculateTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
+} = require('../controllers/orderCtrl');
 const router = express.Router();
 
-router.post('/', verifyToken, authorizedAdmin, createOrder);
-
+router.post('/', verifyToken, createOrder);
+router.get('/', verifyToken, authorizedAdmin, getAllOrders);
+router.get('/mine', verifyToken, getUserOrders);
+router.get('/total-orders', countTotalOrders);
+router.get('/total-sales', calculateTotalSales);
+router.get('/total-sales-by-date', calculateTotalSalesByDate);
+router.get('/:id', verifyToken, findOrderById);
+router.put('/:id/pay', verifyToken, markOrderAsPaid);
+router.put('/:id/deliver', verifyToken, authorizedAdmin, markOrderAsDelivered);
 module.exports = router;
